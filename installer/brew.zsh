df042791c287d6ed2e5533a5944e34d05c480f47
herberttn <htnetooo+github@gmail.com>
2021-10-21 18:00:01 -0300

    Move to brew bundle

--- installer/brew.zsh
+++ installer/brew.zsh
@@ -1,8 +1,5 @@
 #!/usr/bin/env zsh
 
-source $(dirname ${0:A})/brew-casks.zsh
-source $(dirname ${0:A})/brew-formulae.zsh
-
 sudo -v
 
 if which brew &> /dev/null; then
@@ -12,30 +9,5 @@ else
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-echo
-echo brew: updating
-brew update
-
-echo
-echo brew: checking outdated casks
-brew outdated --cask --greedy --verbose
-
-echo
-echo brew: checking outdated formulae
-brew outdated --formula --greedy --verbose
-
-echo
-echo brew: upgrading casks already installed
-brew upgrade --cask $casks
-
-echo
-echo brew: upgrading formulae already installed
-brew upgrade --formula $formulae
-
-echo
-echo brew: installing missing casks
-brew install --cask $casks
-
-echo
-echo brew: installing missing formulae
-brew install --formula $formulae
+echo brew: bundle
+brew bundle
